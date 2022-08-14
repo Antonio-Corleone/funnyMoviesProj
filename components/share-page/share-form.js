1b5f15183c45ec10848888f6871d099cf36586fd
AntonioVu <vuminhtuan3795@gmail.com>
2022-08-14 11:25:42 +0700

    Adding notification share-movie page

--- components/share-page/share-form.js
+++ components/share-page/share-form.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect, Fragment } from 'react';
+
+import Notification from '../ui/notification';
+import NotificationHandler from '../../lib/notification-util';
 import classes from './share-form.module.css';
 
-const handleAddMovie = async (movieInfo)=>{
+const handleAddMovie = async (movieInfo) => {
   const response = await fetch('api/movie/add-movie', {
     method: 'POST',
     body: JSON.stringify(movieInfo),
@@ -15,19 +18,35 @@ const handleAddMovie = async (movieInfo)=>{
   }
 }
 function ShareForm(props) {
-  const {user} = props.author;
+  const { user } = props.author;
   const [enteredInput, setEnteredInput] = useState({
     movie: '',
     title: ''
   })
+  const [requestStatus, setRequestStatus] = useState(null);
+  let notification = NotificationHandler(requestStatus);
+
+  useEffect(() => {
+    if (requestStatus === 'pending' || requestStatus === 'error' || requestStatus === 'success') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      }
+    }
+  }, [requestStatus])
 
   const handleOnSubmit = async (event) => {
     event.preventDefault();
+    setRequestStatus('pending');
     console.log(enteredInput);
     try {
-      await handleAddMovie({...enteredInput,author:user.email});
+      await handleAddMovie({ ...enteredInput, author: user.email });
+      setRequestStatus('success');
     } catch (error) {
       console.log(error);
+      setRequestStatus('error');
     }
     // clear form
     setEnteredInput({
@@ -37,47 +56,56 @@ function ShareForm(props) {
   }
 
   const handleOnChange = (event) => {
-    const {name, value}= event.target;
+    const { name, value } = event.target;
     setEnteredInput({
       ...enteredInput,
       [name]: value
     })
   }
   return (
-    <form className={classes.form} onSubmit={handleOnSubmit}>
-      <fieldset>
-        <legend>Share a Youtube movie</legend>
-        <div className={classes.controls}>
-          <label htmlFor="movie">Youtube URL:</label>
-          <input
-            type="text"
-            name="movie"
-            id="movie"
-            value={enteredInput.movie}
-            onChange={handleOnChange}
-            placeholder="enter your movie url"
-            required
-          />
-        </div>
-        <div className={classes.controls}>
-          <label htmlFor="title">Movie title:</label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={enteredInput.title}
-            onChange={handleOnChange}
-            placeholder="enter your movie title"
-            required
-          />
-        </div>
-        <div className={classes.button}>
-          <button >
-            Share
-          </button>
-        </div>
-      </fieldset>
-    </form>
+    <Fragment>
+      <form className={classes.form} onSubmit={handleOnSubmit}>
+        <fieldset>
+          <legend>Share a Youtube movie</legend>
+          <div className={classes.controls}>
+            <label htmlFor="movie">Youtube URL:</label>
+            <input
+              type="text"
+              name="movie"
+              id="movie"
+              value={enteredInput.movie}
+              onChange={handleOnChange}
+              placeholder="enter your movie url"
+              required
+            />
+          </div>
+          <div className={classes.controls}>
+            <label htmlFor="title">Movie title:</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={enteredInput.title}
+              onChange={handleOnChange}
+              placeholder="enter your movie title"
+              required
+            />
+          </div>
+          <div className={classes.button}>
+            <button >
+              Share
+            </button>
+          </div>
+        </fieldset>
+      </form>
+      {/* Add notification */}
+      {notification &&
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />}
+    </Fragment>
   )
 }
 
