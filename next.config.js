033c7466a71658525c5f69b213ae365880c401e4
AntonioVu <vuminhtuan3795@gmail.com>
2022-08-14 10:56:16 +0700

    Create env variable in next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+
+module.exports = (phase) => {
+  // env variable for dev
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        LOCAL_API: 'http://localhost:3000/',
+        DB_USERNAME: 'TuanVu',
+        DB_PASSWORD: 'VUMINHTUAN371995',
+        DB_CLUSTER: '@tuanvu',
+        DB_DATABASE_NAME: 'my-funny-movie'
+      }
+    }
+  }
+    // env variable for production
+  return {
+    env: {
+      PROD_API: 'https://funny-movies-proj-ju8uyjxq0-antonio-corleone.vercel.app/',
+      DB_USERNAME: 'TuanVu',
+        DB_PASSWORD: 'VUMINHTUAN371995',
+        DB_CLUSTER: '@tuanvu',
+        DB_DATABASE_NAME: 'my-funny-movie'
+    }
+  }
+}
