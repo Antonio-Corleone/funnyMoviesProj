a3e1c6bde3161cfe74fd3ec152c16c3fb0f94632
AntonioVu <vuminhtuan3795@gmail.com>
2022-08-14 09:40:11 +0700

    Improve UI mainNavigation

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,10 +1,20 @@
+import Image from 'next/image';
 import { Fragment } from 'react';
 import classes from './logo.module.css'
 
 export default function Logo() {
   return (
-    <Fragment>
-        <h1 className={classes.logo}>Funny Movies</h1>
-    </Fragment>
+    <div className={classes.content}>
+      <div className={classes.img}>
+        <Image
+          src="/img/home.jpg"
+          alt="Logo"
+          width={60}
+          height={40}
+        />
+      </div>
+      <h1 className={classes.logo}>Funny Movies</h1>
+      <div className={classes.clear}></div>
+    </div>
   )
 }
